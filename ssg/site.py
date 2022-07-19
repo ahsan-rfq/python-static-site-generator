c53e32b47184f5b3628111a5160efc186cf56b4f
ahsan-rfq <69163223+ahsan-rfq@users.noreply.github.com>
2022-07-19 00:45:39 -0400

    Completed module

---
+++ ssg/site.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+class Site:
+    Site(self,source,dest)
+    self.source = Path(source)
+    self.dest = Path(dest)
+
+    def create_dir(self,path):
+        directory = self.dest
+        
